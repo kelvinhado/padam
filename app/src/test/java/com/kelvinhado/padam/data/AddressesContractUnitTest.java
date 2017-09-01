96906fa2ffc5471b8f67f1c3982e6c91b00d1d6e
kelvinhado <kelvinhado@outlook.com>
2017-09-01 20:37:02 +0200

    fix contract unit test

--- app/src/test/java/com/kelvinhado/padam/data/AddressesContractUnitTest.java
+++ app/src/test/java/com/kelvinhado/padam/data/AddressesContractUnitTest.java
@@ -1,5 +1,6 @@
 package com.kelvinhado.padam.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 import org.junit.Test;
@@ -38,9 +39,10 @@ public class AddressesContractUnitTest {
         assertEquals("Cannot find inner class to complete unit test", 1, innerClasses.length);
         Class entryClass = innerClasses[0];
         Field[] allFields = entryClass.getDeclaredFields();
-        assertEquals("There should be exactly 4 String members in the inner class", 4, allFields.length);
+        assertEquals("There should be exactly 4 String members in the inner class", 5, allFields.length);
         for (Field field : allFields) {
-            assertTrue("All members in the contract class should be Strings", field.getType() == String.class);
+            assertTrue("All members in the contract class should be Strings of Uri",
+                    field.getType() == String.class || field.getType() == Uri.class);
             assertTrue("All members in the contract class should be final", Modifier.isFinal(field.getModifiers()));
             assertTrue("All members in the contract class should be static", Modifier.isStatic(field.getModifiers()));
         }
